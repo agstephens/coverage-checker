bdd6133ec1386e987cf4006273c9907e21653460
Ag Stephens <ag.stephens@stfc.ac.uk>
2018-11-09 13:44:54 +0000

    Got first checker working with first tests

--- coverage_checker/coverage_checker.py
+++ coverage_checker/coverage_checker.py
@@ -1,3 +1,127 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import os
+import os.path as op
+
+
+class DirScanner(object):
+
+    def __init__(self, top_dir, ignores=None, ignore_symlinks=False, ignore_files=False,
+                        ignore_dirs=False, ignore_empty_dirs=False):
+
+        # Check top_dir is valid at start
+        if not op.isdir(top_dir):
+            raise Exception('Top-level item is not a valid directory: {}'.format(top_dir))
+
+        self.ignores = ignores or []
+        self.ignore_symlinks = ignore_symlinks
+        self.ignore_files = ignore_files
+        self.ignore_dirs = ignore_dirs
+        self.ignore_empty_dirs = ignore_empty_dirs
+
+        self.files = []
+        self.dirs = []
+        self.empties = []
+
+        self._scan(top_dir)
+        self._sort_results()
+
+
+    def _scan(self, dr):
+        items = os.listdir(dr)
+
+        # Test for empty directory
+        if len(items) == 0:
+            if not self.ignore_empty_dirs:
+                self.empties.append(dr)
+            return
+
+        subdirs_found = False
+
+        for item in items:
+            if item in self.ignores:
+                continue
+
+            path = op.join(dr, item)
+
+            if op.isdir(path):
+                subdirs_found = True
+                self._scan(path)
+
+            elif op.islink(path) and self.ignore_symlinks:
+                continue
+
+            elif not self.ignore_files:
+                self.files.append(path)
+
+        if not subdirs_found and not self.ignore_dirs:
+            print("LLLL {}".format(dr))
+            self.dirs.append(dr)
+
+
+    def _sort_results(self):
+        self.files = sorted(self.files)
+        self.dirs = sorted(self.dirs)
+        self.empties = sorted(self.empties)
+
+
+def _get_files_and_dirs(top_dir, ignores=None, ignore_symlinks=False, ignore_files=False,
+                        ignore_dirs=False, ignore_empty_dirs=False):
+
+    scanner = DirScanner(top_dir, ignores=ignores, ignore_symlinks=ignore_symlinks,
+                         ignore_files=ignore_files, ignore_dirs=ignore_dirs, ignore_empty_dirs=ignore_empty_dirs)
+
+    return scanner.files, scanner.dirs, scanner.empties
+
+
+def check_coverage(top_dir, depth, ignores=None, ignore_symlinks=False, ignore_files=False,
+                   ignore_dirs=False, ignore_empty_dirs=False):
+    files, end_dirs, empty_dirs = _get_files_and_dirs(top_dir, ignores=ignores, ignore_symlinks=ignore_symlinks,
+                                            ignore_files=ignore_files, ignore_dirs=ignore_dirs)
+
+    dir_errors = []
+    file_errors = []
+
+    for dr in end_dirs:
+        items = dr.strip(os.sep).split(os.sep)
+
+        if len(items) != depth:
+            dir_errors.append(dr)
+
+    for fpath in files:
+        if 0:
+            file_errors.append(fpath)
+
+    report = ''
+
+    resp = [0, 0, 0, '']
+
+    if dir_errors:
+        report += 'Directories found that do not match expected depth:\n'
+
+        for dr in dir_errors:
+            resp[0] += 1
+            report += '\t{}\n'.format(dr)
+
+    if file_errors:
+        report += '\nNon-uniform files found:\n'
+
+        for fpath in file_errors:
+            resp[1] += 1
+            report += '\t{}\n'.format(fpath)
+
+    if empty_dirs:
+        report += '\nThe following empty directories were found:\n'
+
+        for empty in empty_dirs:
+            resp[2] += 1
+            report += '\t{}\n'.format(empty)
+
+    resp[3] = report
+    return resp
+
+
+
+
--- tests/test_coverage_checker.py
+++ tests/test_coverage_checker.py
@@ -4,10 +4,11 @@
 """Tests for `coverage_checker` package."""
 
 import pytest
+import os
 
 from click.testing import CliRunner
 
-from coverage_checker import coverage_checker
+from coverage_checker.coverage_checker import check_coverage
 from coverage_checker import cli
 
 
@@ -36,3 +37,37 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_check_coverage_success_1(tmpdir):
+    top_dir = 'top_dir'
+    base = tmpdir.mkdir(top_dir)
+    paths = ('a/b/c/d/1.dat', 'a/b/c/d/2.dat')
+    files = []
+
+    for path in paths:
+        files.append(base.join(path))
+        files[-1].write('data', ensure=True)
+
+    tmp_dir = str(files[0].realpath()).replace(os.path.join(top_dir, paths[0]), '')
+    os.chdir(tmp_dir)
+    resp = check_coverage(top_dir, depth=5)
+    assert(resp == [0, 0, 0, ''])
+
+
+def test_check_coverage_success_2(tmpdir):
+    top_dir = 'top_dir'
+    base = tmpdir.mkdir(top_dir)
+    paths = ('a1/b/c/d/1.dat', 'a2/b/c/d/2.dat')
+    files = []
+
+    for path in paths:
+        files.append(base.join(path))
+        files[-1].write('data', ensure=True)
+
+    tmp_dir = str(files[0].realpath()).replace(os.path.join(top_dir, paths[0]), '')
+    os.chdir(tmp_dir)
+    resp = check_coverage(top_dir, depth=4)
+    assert(resp[0] == 2), resp
+
+
